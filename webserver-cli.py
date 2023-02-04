6baf00976c363da13f2e92fe98f5f7368191304b
valuex <gm2wie@gmail.com>
2023-02-04 15:01:52 +0800

    Add search engine
    添加搜索引擎的配置

--- webserver-cli.py
+++ webserver-cli.py
@@ -7,7 +7,18 @@ from urllib.parse import urlparse,unquote
 import json
 
 PORT = 8002
+def get_cmd_list(filename):
+    file=open(filename,'r',encoding='utf-8')
+    dic=json.load(file)
+    file.close()
+    return dic
+CMDList=get_cmd_list('Config.json')
+EngineList=get_cmd_list('SearchEngine.json')
 
+def default_search(SearchKw):
+    DefaultEngine= EngineList["Default"] 
+    SearchCMD=DefaultEngine.replace("{SearchKw}",SearchKw)
+    os.startfile(SearchCMD)
 def ip_address_helper():
     try:
         host_name = socket.gethostname()
@@ -15,14 +26,6 @@ def ip_address_helper():
         return host_ip
     except:
         return "IP Error!"
-# def get_cmd_list():
-#     file=open('test.json','r',encoding='utf-8')
-#     dic=json.load(file)
-#     file.close()
-#     newDict = {k.upper():v for k,v in dic.items()}  #Convert dictionary  keys to uppercase
-#     return newDict
-# CMDList=get_cmd_list()
-
 class MyRequestHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -30,26 +33,50 @@ class MyRequestHandler(SimpleHTTPRequestHandler):
         post_data = post_data.replace("。","")
         print(unquote(post_data))
         keys = [key.split('=')[1] for key in post_data.split('&')]
+        CMDContent=''.join(keys[1])
         if self.path in['/', '/index.html']:
-            if "OpenURL" in keys[0]:
-                WebUrl=keys[1].replace("%3A%2F%2F","://")
-                WebUrl=WebUrl.replace("%2F","/")
-                os.startfile(WebUrl)
-            elif "JsonURL" in keys[0]:
-                os.startfile(keys[1])
+            if "JsonURL" in keys[0]:
+                if "http" in CMDContent:
+                    os.startfile(CMDContent)
+                elif "搜索" in CMDContent:
+                    self.SearchKeywords(CMDContent)
+                else:
+                    self.search_CDM("JsonURL", CMDContent)
             elif "SendKeys" in keys[0]:
-                CMDContent=unquote(keys[1])
+                CMDContent=unquote(CMDContent)
                 KeySeq=CMDContent.split('||')  
                 for iKey in KeySeq:
                     keys = iKey.lower().split('+') #in the format of list['ctrl','w']
                     hotkey(*keys)  # convert list into string sequence
             elif "OSCMD" in keys[0]:
-                os.startfile(keys[1])
+                try:
+                    os.startfile(CMDContent)
+                except:
+                    self.search_CDM("OSCMD", CMDContent)
+                else:
+                    print("OSCMD" + CMDContent + "excuted")
             else:
                 print(post_data + "Command not defined")
 
         self.do_GET()
-
+    def search_CDM(self,InCMDType,keyword):
+        CMDExcuted=0
+        for iDic in  CMDList:
+            CMDUniqueName=iDic['CMDUniqueName']
+            CMDContent=iDic['CMDContent']
+            CMDNickName=iDic['CMDNickName']  
+            CmdType=iDic['CmdType']
+            if keyword in CMDUniqueName and InCMDType in CmdType:
+                os.startfile(CMDContent)
+                CMDExcuted=1
+                break
+            elif any(keyword in s for s in CMDNickName) and InCMDType in CmdType:
+                os.startfile(CMDContent)
+                CMDExcuted=1
+                break        
+        if CMDExcuted==0:
+            default_search(keyword)
+            # os.startfile("https://www.google.com/search?q="+keyword)
     def do_GET(self):
         query = urlparse(self.path).query
         if query!="":
@@ -62,6 +89,31 @@ class MyRequestHandler(SimpleHTTPRequestHandler):
                 return
         super().do_GET()
 
+    def SearchKeywords(self,keyword):
+        SearchKwPos=keyword.find("搜索")
+        DefaultEngine= EngineList["Default"] 
+
+        if SearchKwPos==-1:
+            SearchKw=keyword
+            SearchCMD=DefaultEngine.replace("{SearchKw}",SearchKw)
+            os.startfile(SearchCMD)
+            return
+        else:
+            SearchKw=keyword[SearchKwPos+2:]
+
+        FindEngine=0
+        for EngineName,Engine in EngineList.items():
+            KeyWd4Engine=EngineName+"搜索"
+            KeyWd4Engine=KeyWd4Engine.lower()
+            if KeyWd4Engine in keyword.lower():
+                SearchCMD=Engine.replace("{SearchKw}",SearchKw)
+                os.startfile(SearchCMD)
+                FindEngine=1
+                break
+        if FindEngine==0:
+                default_search(SearchKw)
+                # SearchCMD=DefaultEngine.replace("{SearchKw}",SearchKw)
+                # os.startfile(SearchCMD)
 
     def send_local(self, path):
         if path == '/favicon.png':
@@ -74,7 +126,6 @@ class MyRequestHandler(SimpleHTTPRequestHandler):
                 super().do_GET()
             else:
                 print('No index.html in the folder')
-        # get_cmd_list()
 
     def send_simple_response(self, content, ctype):
         self.send_response(200)
